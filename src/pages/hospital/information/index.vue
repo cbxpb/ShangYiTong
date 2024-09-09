d770e4ab74abddc143f20500f69c16ab4d513faf
cbxpb <2093362891@qq.com>
2024-09-09 20:13:42 +0800

    预约通知、停诊信息、搜索取消页面完成

--- src/pages/hospital/information/index.vue
+++ src/pages/hospital/information/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>信息</div>
+  <div>
+    <div class="info">
+      <h1>{{ hospitalInfoStore.hospitalInfo?.hospital?.hosname }}停诊信息</h1>
+      <el-empty description="暂无信息" />
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
-<style lang="scss" scoped></style>
+
+<script setup lang="ts">
+  //引入医院详情仓库的数据
+  import { useHospitalInfoStore } from "@/store/hospitalInfo"
+  const hospitalInfoStore = useHospitalInfoStore()
+</script>
+
+<style scoped lang="scss">
+  .info {
+    h1 {
+      text-align: center;
+      font-size: 30px;
+    }
+  }
+</style>
--- src/pages/hospital/search/index.vue
+++ src/pages/hospital/search/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    搜索
+    <div class="info">
+      <h1>{{ hospitalInfoStore.hospitalInfo?.hospital?.hosname }}查询/取消</h1>
+      <el-empty description="暂无信息" />
+    </div>
   </div>
 </template>
-<script setup lang="ts">
 
+<script setup lang="ts">
+  //引入医院详情仓库的数据
+  import { useHospitalInfoStore } from "@/store/hospitalInfo"
+  const hospitalInfoStore = useHospitalInfoStore()
 </script>
-<style lang="scss" scoped>
-  
+
+<style scoped lang="scss">
+.info {
+  h1 {
+    text-align: center;
+    font-size: 30px;
+  }
+}
 </style>
