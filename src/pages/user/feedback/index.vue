7d86943bd95074c9bfbb9d5ad2c18780be1f39f5
cbxpb <2093362891@qq.com>
2024-09-17 18:47:40 +0800

    意见反馈、账号信息完成

--- src/pages/user/feedback/index.vue
+++ src/pages/user/feedback/index.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>222</div>
-  </template>
-  <script setup lang="ts"></script>
-  <style lang="scss" scoped></style>
+  <div>
+    <div class="info">
+      <h1>意见反馈</h1>
+      <el-empty description="暂无信息" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+  .info {
+    h1 {
+      text-align: center;
+      font-size: 30px;
+    }
+  }
+</style>
--- src/pages/user/profile/index.vue
+++ src/pages/user/profile/index.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>114441</div>
-  </template>
-  <script setup lang="ts"></script>
-  <style lang="scss" scoped></style>
+  <div>
+    <div class="info">
+      <h1>账号信息</h1>
+      <el-empty description="暂无信息" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+  .info {
+    h1 {
+      text-align: center;
+      font-size: 30px;
+    }
+  }
+</style>
